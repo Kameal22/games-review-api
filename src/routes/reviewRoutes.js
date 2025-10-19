172a3a09fa1ed14c51715937ff3eac090a79f43e
KamilCz96 <kamil@lifeinmobile.com>
2025-10-19 12:13:26 +0200

    Routes fix

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -14,10 +14,10 @@ const router = Router();
 
 router.get("/", requireAuth, getReviews); // GET /api/reviews
 router.get("/me", requireAuth, listMyReviews); // GET /api/reviews/me
+router.get("/highest-score", requireAuth, fetchAndSortTenByHighestScore); // GET /api/reviews/highest-score
+router.get("/check-exists/:gameId", requireAuth, checkReviewExists); // GET /api/reviews/check-exists/:gameId
 router.post("/", requireAuth, createReview); // POST /api/reviews
 router.patch("/:id", requireAuth, updateReview); // PATCH /api/reviews/:id
 router.get("/:id", requireAuth, getSingleReview); // GET /api/reviews/:id
-router.get("/check-exists/:gameId", requireAuth, checkReviewExists); // GET /api/reviews/check-exists/:gameId
-router.get("/highest-score", requireAuth, fetchAndSortTenByHighestScore); // GET /api/reviews/highest-score
 
 export default router;
